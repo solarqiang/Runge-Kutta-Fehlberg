e93cb4e67fade1a0859233683afd3953f30e030d
Solar Qiang <qiang@Solars-N56VZ.local>
2016-04-13 00:26:31 +0800

    Reformat code style (Google Style)

--- include/orbit_ellipse_2d.hpp
+++ include/orbit_ellipse_2d.hpp
@@ -14,77 +14,78 @@
 #include <cmath>
 #include <stdexcept>
 
-template<class T>
-class OrbitEllipse2D{
-private:
-    T AngularMomentum();
-    T Velocity();
-    T SemiMajorAxis();
-    T GetEnergy();
-    T GetPeriod();
-    T X;                        // x
-    T Y;                        // y
-    T V_x;                      // V x
-    T V_y;                      // V y
-    T R;                        // R
-    T Ax;                       // Semimajor Axis
-    T V_r;                      // radial velocity
-    T Theta;                    // true anomaly
-    T AngVel;                   // angular velocity
-    T V;                        // velocity
-    T AngMom;                   // angular momentum
-    T Ener;                     // energy
-    T Peri;                     // Period
-    T Miu;                      // miu
-    void XY2RTheta();
-public:
-    OrbitEllipse2D(T x, T y, T v_x, T v_y, T miu);
-    T Energy(){return Ener;}
-    T Period(){return Peri;}
+template <class T>
+class OrbitEllipse2D {
+ private:
+  T AngularMomentum();
+  T Velocity();
+  T SemiMajorAxis();
+  T GetEnergy();
+  T GetPeriod();
+  T X;       // x
+  T Y;       // y
+  T V_x;     // V x
+  T V_y;     // V y
+  T R;       // R
+  T Ax;      // Semimajor Axis
+  T V_r;     // radial velocity
+  T Theta;   // true anomaly
+  T AngVel;  // angular velocity
+  T V;       // velocity
+  T AngMom;  // angular momentum
+  T Ener;    // energy
+  T Peri;    // Period
+  T Miu;     // miu
+  void XY2RTheta();
+
+ public:
+  OrbitEllipse2D(T x, T y, T v_x, T v_y, T miu);
+  T Energy() { return Ener; }
+  T Period() { return Peri; }
 };
 
-// 
+//
 // template<class T>
 // T OrbitEllipse2D<T>::Velocity() {
 //     return sqrt(V_r * V_r + AngMom * AngMom / (R * R));
 // }
 
-template<class T>
+template <class T>
 T OrbitEllipse2D<T>::SemiMajorAxis() {
-    return -Miu / (Ener * 2.0);
+  return -Miu / (Ener * 2.0);
 }
 
-template<class T>
+template <class T>
 T OrbitEllipse2D<T>::GetEnergy() {
-    T energy = V * V / 2.0 - Miu / R;
-    if (energy > 0) {
-        throw std::invalid_argument("Energy greater than zero!");
-    }
-    return energy;
+  T energy = V * V / 2.0 - Miu / R;
+  if (energy > 0) {
+    throw std::invalid_argument("Energy greater than zero!");
+  }
+  return energy;
 }
 
-template<class T>
+template <class T>
 T OrbitEllipse2D<T>::GetPeriod() {
-    return 2 * atan(1) * 4 * sqrt(pow(Ax, 3.0) / Miu);
+  return 2 * atan(1) * 4 * sqrt(pow(Ax, 3.0) / Miu);
 }
 
-template<class T>
+template <class T>
 OrbitEllipse2D<T>::OrbitEllipse2D(T x, T y, T V_x, T V_y, T miu) {
-    X = x, Y = y, V_x = V_x, V_y = V_y;
-    Miu     = miu;
+  X = x, Y = y, V_x = V_x, V_y = V_y;
+  Miu = miu;
 
-    R       = sqrt(X * X + Y * Y);
-    V       = sqrt(V_x * V_x + V_y * V_y);
-    Theta   = atan2(Y, X);
-    T alpha = acos((X * V_x + Y * V_y) / (R * V));
-    V_r     = V * cos(alpha);
+  R = sqrt(X * X + Y * Y);
+  V = sqrt(V_x * V_x + V_y * V_y);
+  Theta = atan2(Y, X);
+  T alpha = acos((X * V_x + Y * V_y) / (R * V));
+  V_r = V * cos(alpha);
 
-    AngVel  = V * sin(alpha) / R;
-    AngMom  = V * sin(alpha) * R;
+  AngVel = V * sin(alpha) / R;
+  AngMom = V * sin(alpha) * R;
 
-    Ener    = GetEnergy();
-    Ax      = SemiMajorAxis();
-    Peri    = GetPeriod();
+  Ener = GetEnergy();
+  Ax = SemiMajorAxis();
+  Peri = GetPeriod();
 }
 
-#endif  /* ORBIT_ELLIPSE_2D_HPP_ */
+#endif /* ORBIT_ELLIPSE_2D_HPP_ */
--- include/rkf78.hpp
+++ include/rkf78.hpp
@@ -8,223 +8,216 @@
 #ifndef RKF78_HPP_
 #define RKF78_HPP_
 
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <sstream>
-#include <string>
 #include <stdexcept>
+#include <string>
 using namespace std;
 
-template<class T, int dim>
-class RKF78{
-private:
-    static const T a[13];       // rk78 parameters
-    static const T b[13][12];   // rk78 parameters
-    T K[13][dim];               // runge-kutta parameters
-    T R[dim];                   // error
-    T y[dim];                   // position and velocity
-    T z[13][dim];               // make calculation of Ks easier
-    long step;                  // step count
-    void FindZ(int l);
-    void RungeKuttaParams78();
-    void FindY();
+template <class T, int dim>
+class RKF78 {
+ private:
+  static const T a[13];      // rk78 parameters
+  static const T b[13][12];  // rk78 parameters
+  T K[13][dim];              // runge-kutta parameters
+  T R[dim];                  // error
+  T y[dim];                  // position and velocity
+  T z[13][dim];              // make calculation of Ks easier
+  long step;                 // step count
+  void FindZ(int l);
+  void RungeKuttaParams78();
+  void FindY();
 
-public:
-    T h;                        // step
-    T t;                        // time
-    T (*f[dim])(T t, T y[dim]); // functions to solve
-    T* GetY();
-    T GetT(){return t;}
-    long GetStep(){return step;}
-    void ResetStep(){step=0;}
-    RKF78<T,dim>& SetT(T tnow){t=tnow;return *this;}
-    T hnew(T hmax, T hmin, T q);
-    RKF78<T,dim>& SetY(T iny[dim]);
-    void rkf78(T hmax, T hmin,  T TOL);
-    void solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
-               const char *filename);
-    RKF78<T,dim>(): step(0) {}
-};
-
-
-template<class T, int dim>
-T* RKF78<T, dim>::GetY()
-{
-    return y;
-}
-template<class T, int dim>
-RKF78<T,dim>& RKF78<T, dim>::SetY(T iny[dim])
-{
-    for(int i=0;i<dim;++i)
-    {
-        y[i]=iny[i];
-    }
+ public:
+  T h;                         // step
+  T t;                         // time
+  T (*f[dim])(T t, T y[dim]);  // functions to solve
+  T* GetY();
+  T GetT() { return t; }
+  long GetStep() { return step; }
+  void ResetStep() { step = 0; }
+  RKF78<T, dim>& SetT(T tnow) {
+    t = tnow;
     return *this;
-}
-
-template<class T, int dim>
-const T RKF78<T, dim>::a[13] = {
-    0.0, 2.0/27.0, 1.0/9.0, 1.0/6.0, 5.0/12.0, 0.5,
-    5.0/6.0, 1.0/6.0, 2.0/3.0, 1.0/3.0, 1.0, 0.0, 1.0
+  }
+  T hnew(T hmax, T hmin, T q);
+  RKF78<T, dim>& SetY(T iny[dim]);
+  void rkf78(T hmax, T hmin, T TOL);
+  void solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
+             const char* filename);
+  RKF78<T, dim>() : step(0) {}
 };
 
-template<class T, int dim>
+template <class T, int dim>
+T* RKF78<T, dim>::GetY() {
+  return y;
+}
+template <class T, int dim>
+RKF78<T, dim>& RKF78<T, dim>::SetY(T iny[dim]) {
+  for (int i = 0; i < dim; ++i) {
+    y[i] = iny[i];
+  }
+  return *this;
+}
+
+template <class T, int dim>
+const T RKF78<T, dim>::a[13] = {
+    0.0,       2.0 / 27.0, 1.0 / 9.0, 1.0 / 6.0, 5.0 / 12.0, 0.5, 5.0 / 6.0,
+    1.0 / 6.0, 2.0 / 3.0,  1.0 / 3.0, 1.0,       0.0,        1.0};
+
+template <class T, int dim>
 const T RKF78<T, dim>::b[13][12] = {
     {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-    {2.0/27.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-    {1.0/36.0, 1.0/12.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-    {1.0/24.0, 0.0, 1.0/8.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-    {5.0/12.0, 0.0, -25.0/16.0, 25.0/16.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-     0.0, 0.0, },
-    {1.0/20.0, 0.0, 0.0, 1.0/4.0, 1.0/5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    {2.0 / 27.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {1.0 / 36.0, 1.0 / 12.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {1.0 / 24.0, 0.0, 1.0 / 8.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {
+        5.0 / 12.0, 0.0, -25.0 / 16.0, 25.0 / 16.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+    },
+    {1.0 / 20.0, 0.0, 0.0, 1.0 / 4.0, 1.0 / 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
      0.0},
-    {-25.0/108.0, 0.0, 0.0, 125.0/108.0, -65.0/27.0, 125.0/54.0, 0.0,
+    {-25.0 / 108.0, 0.0, 0.0, 125.0 / 108.0, -65.0 / 27.0, 125.0 / 54.0, 0.0,
      0.0, 0.0, 0.0, 0.0, 0.0},
-    {31.0/300, 0.0, 0.0, 0.0, 61.0/225.0, -2.0/9.0, 13.0/900.0, 0.0,
+    {31.0 / 300, 0.0, 0.0, 0.0, 61.0 / 225.0, -2.0 / 9.0, 13.0 / 900.0, 0.0,
      0.0, 0.0, 0.0, 0.0},
-    {2.0, 0.0, 0.0, -53.0/6.0, 704.0/45.0, -107.0/9.0, 67.0/90.0, 3.0,
+    {2.0, 0.0, 0.0, -53.0 / 6.0, 704.0 / 45.0, -107.0 / 9.0, 67.0 / 90.0, 3.0,
      0.0, 0.0, 0.0, 0.0},
-    {-91.0/108.0, 0.0, 0.0, 23.0/108.0, -976.0/135.0, 311.0/54.0, -19.0/60.0,
-     17.0/6.0, -1.0/12.0, 0.0, 0.0, 0.0},
-    {2383.0/4100, 0.0, 0.0, -341.0/164.0, 4496.0/1025.0, -301.0/82.0,
-     2133.0/4100.0, 45.0/82.0, 45.0/164.0, 18.0/41.0, 0.0, 0.0},
-    {3.0/205.0, 0.0, 0.0, 0.0, 0.0, -6.0/41.0, -3.0/205.0, -3.0/41.0,
-     3.0/41.0, 6.0/41.0, 0.0, 0.0},
-    {-1777.0/4100.0, 0.0, 0.0, -341.0/164.0, 4496.0/1025.0, -289.0/82.0,
-     2193.0/4100.0, 51.0/82.0, 33.0/164.0, 12.0/41.0, 0.0, 1.0}
-};
+    {-91.0 / 108.0, 0.0, 0.0, 23.0 / 108.0, -976.0 / 135.0, 311.0 / 54.0,
+     -19.0 / 60.0, 17.0 / 6.0, -1.0 / 12.0, 0.0, 0.0, 0.0},
+    {2383.0 / 4100, 0.0, 0.0, -341.0 / 164.0, 4496.0 / 1025.0, -301.0 / 82.0,
+     2133.0 / 4100.0, 45.0 / 82.0, 45.0 / 164.0, 18.0 / 41.0, 0.0, 0.0},
+    {3.0 / 205.0, 0.0, 0.0, 0.0, 0.0, -6.0 / 41.0, -3.0 / 205.0, -3.0 / 41.0,
+     3.0 / 41.0, 6.0 / 41.0, 0.0, 0.0},
+    {-1777.0 / 4100.0, 0.0, 0.0, -341.0 / 164.0, 4496.0 / 1025.0, -289.0 / 82.0,
+     2193.0 / 4100.0, 51.0 / 82.0, 33.0 / 164.0, 12.0 / 41.0, 0.0, 1.0}};
 
-template<class T, int dim>
+template <class T, int dim>
 void RKF78<T, dim>::FindZ(int l) {
-    T tmp[dim];
-    for (int i=0; i < dim; i++) {
-        tmp[i] = 0;
+  T tmp[dim];
+  for (int i = 0; i < dim; i++) {
+    tmp[i] = 0;
+  }
+  if (l != 0) {
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < l; j++) {
+        tmp[i] += K[j][i] * b[l][j];
+      }
     }
-    if (l != 0) {
-        for (int i=0; i < dim; i++) {
-            for (int j=0; j < l; j++) {
-                tmp[i] += K[j][i] * b[l][j];
-            }
-        }
-    }
-    for (int i=0; i < dim; i++) {
-        z[l][i] = y[i] + tmp[i];
-    }
+  }
+  for (int i = 0; i < dim; i++) {
+    z[l][i] = y[i] + tmp[i];
+  }
 }
 
-template<class T, int dim>
+template <class T, int dim>
 void RKF78<T, dim>::RungeKuttaParams78() {
-    // get runge-kutta parameters
-    for (int j=0; j < 13; j++) {
-        FindZ(j);
-        for (int i=0; i < dim; i++) {
-            K[j][i] = h * (*f[i])(t + h * a[j], z[j]);
-        }
+  // get runge-kutta parameters
+  for (int j = 0; j < 13; j++) {
+    FindZ(j);
+    for (int i = 0; i < dim; i++) {
+      K[j][i] = h * (*f[i])(t + h * a[j], z[j]);
     }
+  }
 }
 
-template<class T, int dim>
+template <class T, int dim>
 void RKF78<T, dim>::FindY() {
-    // get y using runge-kutta parameters
-    for (int i=0; i < dim; i++) {
-        y[i] += K[5][i] * 34.0 / 105.0 +
-            (K[6][i] + K[7][i]) * 9.0 / 35.0 +
+  // get y using runge-kutta parameters
+  for (int i = 0; i < dim; i++) {
+    y[i] += K[5][i] * 34.0 / 105.0 + (K[6][i] + K[7][i]) * 9.0 / 35.0 +
             (K[8][i] + K[9][i]) * 9.0 / 280.0 +
             (K[11][i] + K[12][i]) * 41.0 / 840.0;
-    }
+  }
 }
 
-template<class T, int dim>
+template <class T, int dim>
 T RKF78<T, dim>::hnew(T hmax, T hmin, T q) {
-    // get new h
-    if (q <= 0.1) {
-        h = h * 0.1;
-    }
-    else if (q >= 4.0) {
-        h = h * 4.0;
-    }
-    else {
-        h = h * q;
-    }
-    if (h >= hmax)
-        h = hmax;
-    else if (h < hmin)
-        h = -1;
-    return h;
+  // get new h
+  if (q <= 0.1) {
+    h = h * 0.1;
+  } else if (q >= 4.0) {
+    h = h * 4.0;
+  } else {
+    h = h * q;
+  }
+  if (h >= hmax)
+    h = hmax;
+  else if (h < hmin)
+    h = -1;
+  return h;
 }
 
-template<class T, int dim>
+template <class T, int dim>
 void RKF78<T, dim>::rkf78(T hmax, T hmin, T TOL) {
-    // function to apply the runge-kutta-fehlberg method for one step
-     while(true) {
-        RungeKuttaParams78();  // get Ks
-        for (int i=0; i < dim; i++) {            // finding errors
-            R[i] = fabs(K[0][i] + K[10][i] - K[11][i] - K[12][i])\
-                * (h) * 41.0 / 810.0;
-        }
-        T MaxErr = *max_element(R, R + dim); // maximium value of array R
-        T q=pow(TOL / (MaxErr * 2.0), 1.0 / 7.0);
-        if (MaxErr < TOL) {
-            t += h;
-            FindY();         // get Ys
-            ++step;
-            break;
-        }
-        h = hnew(hmax, hmin, q); // new h
-        if (h < 0){                     // error handling
-            throw invalid_argument("Minimum h exceeded!");
-        }
+  // function to apply the runge-kutta-fehlberg method for one step
+  while (true) {
+    RungeKuttaParams78();            // get Ks
+    for (int i = 0; i < dim; i++) {  // finding errors
+      R[i] = fabs(K[0][i] + K[10][i] - K[11][i] - K[12][i]) * (h)*41.0 / 810.0;
     }
+    T MaxErr = *max_element(R, R + dim);  // maximium value of array R
+    T q = pow(TOL / (MaxErr * 2.0), 1.0 / 7.0);
+    if (MaxErr < TOL) {
+      t += h;
+      FindY();  // get Ys
+      ++step;
+      break;
+    }
+    h = hnew(hmax, hmin, q);  // new h
+    if (h < 0) {              // error handling
+      throw invalid_argument("Minimum h exceeded!");
+    }
+  }
 }
 
-template<class T, int dim>
+template <class T, int dim>
 void RKF78<T, dim>::solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
-           const char *filename) {
-    // function to apply the runge-kutta-fehlberg method
-    // open a file in write mode.
-    ofstream outfile;
-    outfile.open(filename, ios::out);
-    t = begin;             // begin of t
-    h = hmax;              // begin of h
-    step = 0;              // step
-    SetY(y0);
-    // output header
-    cout<<setw(28)<<"t"<<setw(28)<<"h";
-    outfile<<setw(28)<<"t"<<setw(28)<<"h";
-    for (int i=0; i < dim; i++) {
-        // convert number to string
-        ostringstream convert;
-        convert << i;
-        string yi = "y" + convert.str();
-        cout<<setw(28)<<yi;
-        outfile<<setw(28)<<yi;
+                          const char* filename) {
+  // function to apply the runge-kutta-fehlberg method
+  // open a file in write mode.
+  ofstream outfile;
+  outfile.open(filename, ios::out);
+  t = begin;  // begin of t
+  h = hmax;   // begin of h
+  step = 0;   // step
+  SetY(y0);
+  // output header
+  cout << setw(28) << "t" << setw(28) << "h";
+  outfile << setw(28) << "t" << setw(28) << "h";
+  for (int i = 0; i < dim; i++) {
+    // convert number to string
+    ostringstream convert;
+    convert << i;
+    string yi = "y" + convert.str();
+    cout << setw(28) << yi;
+    outfile << setw(28) << yi;
+  }
+  cout << endl;
+  outfile << endl;
+  for (; t < end;) {
+    rkf78(hmax, hmin, TOL);  // calculate one step
+    step++;                  // step plus one
+    // output result
+    cout << setiosflags(ios::scientific) << setprecision(18) << setw(28) << t
+         << setprecision(18) << setw(28) << h;
+    outfile << setiosflags(ios::scientific) << setprecision(18) << setw(28) << t
+            << setprecision(18) << setw(28) << h;
+    for (int i = 0; i < dim; i++) {
+      cout << setiosflags(ios::scientific) << setprecision(18) << setw(28)
+           << y[i];
+      outfile << setiosflags(ios::scientific) << setprecision(18) << setw(28)
+              << y[i];
     }
-    cout<<endl;
-    outfile<<endl;
-    for (;t < end;) {
-        rkf78(hmax, hmin,  TOL); // calculate one step
-        step++;                               // step plus one
-        // output result
-        cout<<setiosflags(ios::scientific)
-            <<setprecision(18)<<setw(28)<<t
-            <<setprecision(18)<<setw(28)<<h;
-        outfile<<setiosflags(ios::scientific)
-               <<setprecision(18)<<setw(28)<<t
-               <<setprecision(18)<<setw(28)<<h;
-        for (int i=0; i < dim; i++) {
-            cout<<setiosflags(ios::scientific)<<setprecision(18)
-                <<setw(28)<<y[i];
-            outfile<<setiosflags(ios::scientific)<<setprecision(18)
-                   <<setw(28)<<y[i];
-        }
-        cout<<endl;
-        outfile<<endl;
-    }
-    outfile.close();                    // close the opened file.
-    cout<<"Procedure completed!"<<endl; // end
+    cout << endl;
+    outfile << endl;
+  }
+  outfile.close();                         // close the opened file.
+  cout << "Procedure completed!" << endl;  // end
 }
 
-#endif  /* RKF78_HPP_ */
+#endif /* RKF78_HPP_ */
